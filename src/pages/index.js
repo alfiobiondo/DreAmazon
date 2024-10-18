d91e976e462a10404506497bbc54b6fdd68d3fb4
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2024-10-18 13:11:46 +0200

    add SSR and fetch fakestoreapi products

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Head from 'next/head';
 import Header from '@/components/Header';
 import Banner from '@/components/Banner';
+import ProductFeed from '@/components/ProductFeed';
 
-const Home = () => {
+const Home = ({ products }) => {
 	return (
 		<div className='bg-gray-100'>
 			<Head>
@@ -15,10 +16,31 @@ const Home = () => {
 			<main className='max-w-screen-2xl mx-auto'>
 				<Banner />
 
-				{/* ProductFeed */}
+				<ProductFeed products={products} />
 			</main>
 		</div>
 	);
 };
 
 export default Home;
+
+export async function getServerSideProps(context) {
+	try {
+		const products = await fetch('https://fakestoreapi.com/products').then(
+			(res) => res.json()
+		);
+
+		return {
+			props: {
+				products,
+			},
+		};
+	} catch (error) {
+		console.log('getServerSideProps error', error);
+		return {
+			props: {
+				products: [], // Return an empty array in case of error
+			},
+		};
+	}
+}
