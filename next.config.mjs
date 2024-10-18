186fdef9d5235f04ac91c338e8127298c0181960
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2024-10-18 10:09:23 +0200

    finish header top portion

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'links.papareact.com',
+				pathname: '**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'fakestoreapi.com',
+				pathname: '**',
+			},
+		],
+	},
 };
 
 export default nextConfig;
