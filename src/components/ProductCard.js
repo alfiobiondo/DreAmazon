82a7b8bf8804b7341ba51fa6c18a0beea51348ea
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2024-10-19 16:00:12 +0200

    adjust ProductCard layout, start ProductFeed layout design

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
-
 import { StarIcon } from '@heroicons/react/24/solid';
-
 import { IntlProvider, FormattedNumber } from 'react-intl';
 import useProductCard from '@/hooks/useProductCard';
 
@@ -10,19 +8,28 @@ const ProductCard = ({ id, title, price, description, category, image }) => {
 	const { rating, hasPrime } = useProductCard();
 
 	return (
-		<article className='relative m-5 bg-white z-30 p-10'>
+		<article className='relative m-5 bg-white z-30 p-10 flex flex-col'>
+			{/* Header: Product image, category, title, rating */}
 			<header className='flex flex-col'>
 				<p className='absolute top-2 right-2 text-xs italic text-gray-400'>
 					{category}
 				</p>
-				<Image
-					src={image}
-					alt='Product image'
-					height={200}
-					width={200}
-					className='object-contain h-50 w-auto m-auto'
-				/>
-				<h4 className='my-3'>{title}</h4>
+
+				{/* Image */}
+				<div className='h-48 w-full flex items-center justify-center'>
+					<Image
+						src={image}
+						alt='Product image'
+						height={200}
+						width={200}
+						className='object-contain h-full w-auto'
+					/>
+				</div>
+
+				{/* Title */}
+				<h4 className='mt-3 mb-1'>{title}</h4>
+
+				{/* Rating */}
 				<div className='flex'>
 					{Array.from({ length: rating }, (_, i) => (
 						<StarIcon key={i} className='h-5 text-yellow-500' />
@@ -30,27 +37,35 @@ const ProductCard = ({ id, title, price, description, category, image }) => {
 				</div>
 			</header>
 
-			<p className='text-xs my-2 line-clamp-2'>{description}</p>
+			{/* Main: Description and price */}
+			<main className='mt-2'>
+				<p className='text-xs my-2 line-clamp-2'>{description}</p>
+				<IntlProvider locale='it' defaultLocale='it'>
+					<FormattedNumber value={price} style='currency' currency='EUR' />
+				</IntlProvider>
+			</main>
 
-			<footer className='flex flex-col'>
-				<div className='mb-5'>
-					<IntlProvider locale='it' defaultLocale='it'>
-						<FormattedNumber value={price} style='currency' currency='EUR' />
-					</IntlProvider>
+			{/* Footer: Prime logo and button */}
+			<footer className='mt-auto'>
+				{/* Prime Badge */}
+				<div className='flex items-center space-x-2 mb-2'>
+					{hasPrime ? (
+						<>
+							<Image
+								src='/images/amazon_prime_icon.png'
+								alt='Prime logo'
+								width={50}
+								height={20}
+							/>
+							<p className='text-xs text-gray-500'>FREE Next-day Delivery</p>
+						</>
+					) : (
+						<div className='w-[50px] h-[20px]' />
+					)}
 				</div>
-				{hasPrime && (
-					<div className='flex items-center space-x-2 -mt-5 mb-2'>
-						<Image
-							src='/images/amazon_prime_icon.png'
-							alt='Prime logo'
-							width={50}
-							height={20}
-						/>
-						<p className='text-xs text-gray-500'>FREE Next-day Delivery</p>
-					</div>
-				)}
 
-				<button className='mt-auto button'>Add to Basket</button>
+				{/* Add to Basket Button */}
+				<button className='button w-full'>Add to Basket</button>
 			</footer>
 		</article>
 	);
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,7 +3,7 @@ import ProductCard from './ProductCard';
 
 const ProductFeed = ({ products }) => {
 	return (
-		<>
+		<section className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 			{products.map(({ id, title, price, description, category, image }) => (
 				<ProductCard
 					key={id}
@@ -15,7 +15,7 @@ const ProductFeed = ({ products }) => {
 					image={image}
 				/>
 			))}
-		</>
+		</section>
 	);
 };
 
