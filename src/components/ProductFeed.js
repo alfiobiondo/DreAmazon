d7d0f161c29b1412d2eff814dc0e5ea587b13cd4
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2024-10-22 20:18:33 +0200

    finish product feed component

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,18 +3,54 @@ import ProductCard from './ProductCard';
 
 const ProductFeed = ({ products }) => {
 	return (
-		<section className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-			{products.map(({ id, title, price, description, category, image }) => (
-				<ProductCard
-					key={id}
-					id={id}
-					title={title}
-					price={price}
-					description={description}
-					category={category}
-					image={image}
-				/>
-			))}
+		<section className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+			{products
+				.slice(0, 4)
+				.map(({ id, title, price, description, category, image }) => (
+					<ProductCard
+						key={id}
+						id={id}
+						title={title}
+						price={price}
+						description={description}
+						category={category}
+						image={image}
+					/>
+				))}
+
+			<section className='md:col-span-full'>
+				<img src='https://links.papareact.com/dyz' alt='advertisement' />
+			</section>
+
+			<div className='md:col-span-2'>
+				{products
+					.slice(4, 5)
+					.map(({ id, title, price, description, category, image }) => (
+						<ProductCard
+							key={id}
+							id={id}
+							title={title}
+							price={price}
+							description={description}
+							category={category}
+							image={image}
+						/>
+					))}
+			</div>
+
+			{products
+				.slice(5, products.length)
+				.map(({ id, title, price, description, category, image }) => (
+					<ProductCard
+						key={id}
+						id={id}
+						title={title}
+						price={price}
+						description={description}
+						category={category}
+						image={image}
+					/>
+				))}
 		</section>
 	);
 };
